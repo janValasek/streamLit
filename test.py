57e808c042fc0d26c748c9ab05f05bf40fa74eb9
Jan <32088829+janValasek@users.noreply.github.com>
2022-07-09 12:15:52 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,37 +1,8 @@
-import pandas as pd
+
 import streamlit as st
-import plotly.express as px
-from PIL import Image
+
 
 st.set_page_config(page_title='Survey Results')
 st.header('Survey Results 2021')
 st.subheader('Was the tutorial helpful?')
 
-### --- LOAD DATAFRAME
-excel_file = 'Survey_Results.xlsx'
-sheet_name = 'DATA'
-
-df = pd.read_excel(excel_file,
-                   sheet_name=sheet_name,
-                   usecols='B:D',
-                   header=3)
-
-df_participants = pd.read_excel(excel_file,
-                                sheet_name= sheet_name,
-                                usecols='F:G',
-                                header=3)
-df_participants.dropna(inplace=True)
-
-# --- STREAMLIT SELECTION
-department = df['Department'].unique().tolist()
-ages = df['Age'].unique().tolist()
-
-
-age_selection = st.slider('Age:',
-                        min_value= min(ages),
-                        max_value= max(ages),
-                        value=(min(ages),max(ages)))
-
-department_selection = st.multiselect('Department:',
-                                    department,
-                                    default=department)
