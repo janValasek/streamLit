fc71c39535eba8e97834d515932ec93ebfeb1dd8
Jan <32088829+janValasek@users.noreply.github.com>
2022-07-09 13:32:17 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ import streamlit as st
 
 
 st.set_page_config(page_title='Survey Results')
-st.header('Survey Results 2021')
+st.header('First streamlit page ')
 st.subheader('Was the tutorial helpful?')
 
